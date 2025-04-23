ec8a0a738c4ee77e5a62643255e4daf4290268d4
ktq2490317@stu.o-hara.ac.jp <ktq2490317@stu.o-hara.ac.jp>
2025-04-23 10:54:05 +0900

    SubjectUpdateAction.javaとSubjectUpdateEcecuteAction.javaの完成

--- src/scoremanager/main/SubjectUpdateAction.java
+++ src/scoremanager/main/SubjectUpdateAction.java
@@ -1,9 +1,6 @@
 // 科目更新
 package scoremanager.main;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -15,37 +12,29 @@ import tool.Action;
 public class SubjectUpdateAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception{
-		@SuppressWarnings("unused")
+
 		Teacher user = this.getUserFromSession(req, res);
+	    if (user == null) {
+	        res.sendRedirect("login.jsp"); // 例：ログイン画面へリダイレクト
+	        return;
+	    }
 
 		// DaoとBeanのインスタンス化
 		SubjectDao subDao = new SubjectDao();
 		Subject subject = new Subject();
 
-		// Map
-		Map<String,String> errors = new HashMap<>();
-
-		// subject初期化
-		subject = null;
 
 		// 画面遷移前から学校コードの取得
-		String schoolCd = req.getParameter("cd");
+		String subjectCd = req.getParameter("subCd");
+		System.out.println(subjectCd);
 
 		// subjectテーブルからデータの取得
-		subject = subDao.get(schoolCd, user.getSchool());
-
-		// 科目が存在しない場合
-		if(subject == null){
-			// 該当する学校コードが存在しない場合
-			errors.put("erros_notFound_cd", "科目が存在していません。");
-			req.setAttribute("errors", errors);
-			req.getRequestDispatcher("SubjectUpdate.action").forward(req, res);
-		}
+		subject = subDao.get(subjectCd, user.getSchool());
+		System.out.println(subject);
 
 		// セット
 		req.setAttribute("now_cd", subject.getCd());
 		req.setAttribute("now_name", subject.getName());
-
 		// フォワード
 		req.getRequestDispatcher("subject_update.jsp").forward(req, res);
 	}
--- src/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -1,18 +1,55 @@
 //科目更新
 package scoremanager.main;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Subject;
 import bean.Teacher;
+import dao.SubjectDao;
 import tool.Action;
 
 public class SubjectUpdateExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception{
-		@SuppressWarnings("unused")
 		Teacher user = this.getUserFromSession(req, res);
 
 
+		// インスタンス化
+		Subject subject = new Subject();
+		SubjectDao subDao = new SubjectDao();
+
+		// Map
+		Map<String,String> errors = new HashMap<>();
+
+
+		// 空の値を設定
+		String inputCd=null;
+		String inputName=null;
+
+		// パラメータ取得
+		inputCd=req.getParameter("cd"); // 科目コード
+		inputName=req.getParameter("name"); // 科目名
+
+		// 科目が存在しない場合
+		if(inputCd == null){
+			// 該当する学校コードが存在しない場合
+			errors.put("erros_notFound_cd", "科目が存在していません。");
+			req.setAttribute("errors", errors);
+			req.getRequestDispatcher("SubjectUpdate.action").forward(req, res);
+		}
+
+		// subjectにセット
+		subject.setCd(inputCd);
+		subject.setName(inputName);
+		subject.setSchool(user.getSchool());
+
+
+		if (subDao.save(subject)) {
+			req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
+		}
 	}
 }
